6ff7062a14d9ae1e05c1f4c59c4285f391a841bc
KillerMaker <emmanuelmtr1234@gmail.com>
2021-06-23 15:35:42 -0400

    Marca ready

---
+++ WebApplication4/Controllers/MarcaController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class MarcaController : Controller,IController
+    {
+        public ActionResult ActualizarOpen(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+
+        public async Task<ActionResult> ActualizarSend(int id)
+        {
+            CMarca marca = new CMarca(id, Request.Form["Descripcion"], int.Parse(Request.Form["estado"]));
+            await marca.Update();
+            return Redirect("https://localhost:44368/Marca/SelectShow");
+        }
+
+        public async Task<ActionResult> EliminarOpen(int id)
+        {
+            ViewData["id"] = id;
+            CMarca marca = null;
+
+            foreach (CMarca mrc in await CMarca.Select($"WHERE ID={id}"))
+                marca = mrc;
+            
+            return View(marca);
+        }
+
+        public async Task<ActionResult> EliminarSend(int id)
+        {
+           await CMarca.Delete(id);
+            return Redirect("https://localhost:44368/Marca/SelectShow");
+        }
+        public ActionResult InsertarOpen() => View();
+
+        public async Task<ActionResult> InsertarSend()
+        {
+            CMarca marca = new CMarca(null,Request.Form["Descripcion"], int.Parse(Request.Form["estado"]));
+            await marca.Insert();
+            return Redirect("https://localhost:44368/Marca/SelectShow");
+        }
+
+        public async Task<ActionResult> SelectShow() => View(await CMarca.Select());
+    }
+}
---
+++ WebApplication4/Models/CMarca.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication4.Models
+{
+    public class CMarca : CEntidad
+    {
+        public int? id { get; set; }
+        public string descripcion { get; set; }
+        public int estado { get; set; }
+
+        private static MySqlConnection connection;
+        private static MySqlCommand command;
+        private static MySqlDataReader reader;
+
+        public CMarca(int? id, string descripcion,int estado)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+            this.estado = estado;
+
+            connection = _connection;
+        }
+        public async override Task<int> Insert()
+        {
+            try
+            {
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"INSERT INTO MARCA(DESCRIPCION,ESTADO)
+                                                    VALUES('{descripcion}',{estado});",connection);
+
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async override Task<int> Update()
+        {
+            try
+            {
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"UPDATE MARCA SET DESCRIPCION='{descripcion}',ESTADO={estado}
+                                                     WHERE ID={id};", connection);
+
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async static Task<int> Delete(int id)
+        {
+            try
+            {
+                connection = _connection;
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($"DELETE FROM MARCA WHERE ID={id}", connection);
+
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async static Task<List<CMarca>> Select(string searchString=null)
+        {
+            try
+            {
+                List<CMarca> marcas = new List<CMarca>();
+                CMarca marca = null;
+
+                connection = _connection;
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"SELECT * FROM MARCA {searchString}", connection);
+                reader = (MySqlDataReader)(await command.ExecuteReaderAsync());
+
+                while (reader.Read())
+                {
+                    marca = new CMarca((int)reader[0], (string)reader[1], (int)reader[2]);
+                    marcas.Add(marca);
+                }
+
+                return marcas;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
