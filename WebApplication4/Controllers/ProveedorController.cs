7f9061a7e8ef15546608f960b84a86b801df50e1
KillerMaker <emmanuelmtr1234@gmail.com>
2021-06-22 21:34:30 -0400

    Proveedor ready

---
+++ WebApplication4/Controllers/ProveedorController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        public ActionResult InsertarOpen() => View();
+        public async Task<ActionResult> InsertarSend()
+        {
+            CProveedor proveedor = new CProveedor(
+                null,
+                Request.Form["Cedula"].ToString(),
+                Request.Form["NombreComercial"].ToString(),
+                int.Parse(Request.Form["Estado"].ToString()));
+
+            await proveedor.Insert();
+            return Redirect("https://localhost:44368/Proveedor/SelectShow");
+        }
+
+        public ActionResult ActualizarOpen(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+        public async Task<ActionResult> ActualizarSend(int id)
+        {
+            CProveedor proveedor = new CProveedor(
+               id,
+               Request.Form["Cedula"].ToString(),
+               Request.Form["NombreComercial"].ToString(),
+               int.Parse(Request.Form["Estado"].ToString()));
+
+            await proveedor.Update();
+            return Redirect("https://localhost:44368/Proveedor/SelectShow");
+        }
+
+        public ActionResult EliminarOpen(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+        public async Task<ActionResult> EliminarSend(int id)
+        {
+            await CProveedor.Delete(id);
+            return Redirect("https://localhost:44368/Proveedor/SelectShow");
+        }
+        public async Task<ActionResult> SelectShow() => View(await CProveedor.Select());
+    }
+}
---
+++ WebApplication4/Models/CProveedor.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication4.Models
+{
+    public class CProveedor : CEntidad
+    {
+        public int? id { get; set; }
+        public string cedula { get; set; }
+        public string nombreComercial {get ; set;}
+        public int estado { get; set; }
+
+        public static MySqlCommand command;
+        public static MySqlDataReader reader;
+        public static MySqlConnection connection;
+
+        public CProveedor(int? id,string cedula,string nombreComercial,int estado)
+        {
+            this.id = id;
+            this.cedula = cedula;
+            this.nombreComercial = nombreComercial;
+            this.estado = estado;
+        }
+
+        public async override Task<int> Insert()
+        {
+            try
+            {
+                connection = _connection;
+
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"INSERT INTO PROVEEDOR
+                                                        (   CEDULA,
+                                                            NOMBRE_COMERCIAL,
+                                                            ESTADO )
+                                            VALUES('{cedula}','{nombreComercial}',{estado});", connection);
+
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public async override Task<int> Update()
+        {
+            try
+            {
+                connection = _connection;
+
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"UPDATE PROVEEDOR SET
+                                                    CEDULA='{cedula}',
+                                                    NOMBRE_COMERCIAL='{nombreComercial}',
+                                                    ESTADO={estado}
+                                                WHERE ID={id};", connection);
+
+                return await command.ExecuteNonQueryAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public async static Task<int> Delete(int id)
+        {
+            try
+            {
+                connection = _connection;
+
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"DELETE FROM PROVEEDOR WHERE ID={id}", connection);
+
+                return await command.ExecuteNonQueryAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public async static Task<List<CProveedor>> Select(string searchString = null)
+        {
+            try
+            {
+                List<CProveedor> proveedores = new List<CProveedor>();
+                CProveedor proveedor = null;
+
+                connection = _connection;
+
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"SELECT * FROM PROVEEDOR {searchString}", connection);
+                reader = (MySqlDataReader)(await command.ExecuteReaderAsync());
+
+                while (reader.Read())
+                {
+                    proveedor = new CProveedor((int)reader[0], (string)reader[1], (string)reader[2], (int)reader[3]);
+                    proveedores.Add(proveedor);
+                }
+
+                return proveedores;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
