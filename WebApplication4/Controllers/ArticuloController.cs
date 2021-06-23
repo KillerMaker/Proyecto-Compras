500d5504f3dea0d4fa38913991a3e76c6d63b584
KillerMaker <emmanuelmtr1234@gmail.com>
2021-06-23 17:29:16 -0400

    Articulo Ready

---
+++ WebApplication4/Controllers/ArticuloController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class ArticuloController : Controller, IController
+    {
+        public ActionResult ActualizarOpen(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+
+        public async Task<ActionResult> ActualizarSend(int id)
+        {
+            CArticulo articulo = new CArticulo
+                (
+                    id,
+                    Request.Form["Descripcion"],
+                    int.Parse(Request.Form["Marca"]),
+                    int.Parse(Request.Form["UnidadMedida"]),
+                    int.Parse(Request.Form["Existencia"]),
+                    int.Parse(Request.Form["Estado"])
+                );
+
+            await articulo.Update();
+            return Redirect("https://localhost:44368/Articulo/SelectShow");
+
+        }
+
+        public async Task<ActionResult> EliminarOpen(int id)
+        {
+            ViewData["id"] = id;
+            CArticulo articulo = null;
+
+            foreach (CArticulo art in await CArticulo.Select($"WHERE ID={id}"))
+                articulo = art;
+
+            return View(articulo);
+        }
+
+        public async Task<ActionResult> EliminarSend(int id)
+        {
+            await CArticulo.Delete(id);
+
+            return Redirect("https://localhost:44368/Articulo/SelectShow");
+        }
+
+        public ActionResult InsertarOpen() => View();
+
+        public async Task<ActionResult> InsertarSend()
+        {
+            CArticulo articulo = new CArticulo
+                (
+                    null,
+                    Request.Form["Descripcion"],
+                    int.Parse(Request.Form["Marca"]),
+                    int.Parse(Request.Form["UnidadMedida"]),
+                    int.Parse(Request.Form["Existencia"]),
+                    int.Parse(Request.Form["Estado"])
+                );
+
+            await articulo.Insert();
+            return Redirect("https://localhost:44368/Articulo/SelectShow");
+        }
+
+        public async Task<ActionResult> SelectShow() => View(await CArticulo.Select());
+    }
+}
---
+++ WebApplication4/Models/CArticulo.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication4.Models
+{
+    public class CArticulo : CEntidad
+    {
+        public int? id { get; set; }
+        public string descripcion { get; set; }
+        public int marca { get; set; }
+        public int unidadMedida { get; set; }
+        public int existencia { get; set; }
+        public int estado { get; set; }
+
+        private static MySqlConnection connection;
+        private static MySqlCommand command;
+        private static MySqlDataReader reader;
+
+        public CArticulo(int? id, string descripcion, int marca, int unidadMedida, int existencia, int estado)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+            this.marca = marca;
+            this.unidadMedida = unidadMedida;
+            this.existencia = existencia;
+            this.estado = estado;
+
+            connection = _connection;
+        }
+
+        public async override Task<int> Insert()
+        {
+            try
+            {
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"INSERT INTO ARTICULO
+                                                (DESCRIPCION,MARCA,UNIDAD_MEDIDA,EXISTENCIA,ESTADO)
+                                                    VALUES('{descripcion}',
+                                                            {marca},
+                                                            {unidadMedida},
+                                                            {existencia},
+                                                            {estado})", connection);
+
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async override Task<int> Update()
+        {
+            try
+            {
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"UPDATE ARTICULO SET
+                                                DESCRIPCION = '{descripcion}',
+                                                MARCA = {marca},
+                                                UNIDAD_MEDIDA = {unidadMedida},
+                                                EXISTENCIA = {existencia},
+                                                ESTADO = {estado} WHERE ID={id}", connection);
+
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async static Task<int> Delete(int id)
+        {
+            try
+            {
+                connection = _connection;
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"DELETE FROM ARTICULO WHERE ID={id}", connection);
+
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async static Task<List<CArticulo>> Select(string searchString = null)
+        {
+            try
+            {
+                List<CArticulo> articulos = new List<CArticulo>();
+                CArticulo articulo = null;
+
+                connection = _connection;
+                if (connection.State.Equals(ConnectionState.Open))
+                    await connection.CloseAsync();
+
+                await connection.OpenAsync();
+
+                command = new MySqlCommand($@"SELECT * FROM ARTICULO {searchString}", connection);
+                reader = (MySqlDataReader)(await command.ExecuteReaderAsync());
+
+                while (reader.Read())
+                {
+                    articulo = new CArticulo
+                       (
+                           (int)reader[0],
+                           (string)reader[1],
+                           (int)reader[2],
+                           (int)reader[3],
+                           (int)reader[4],
+                           (int)reader[5]
+                       );
+
+                    articulos.Add(articulo);
+                }
+
+                return articulos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
